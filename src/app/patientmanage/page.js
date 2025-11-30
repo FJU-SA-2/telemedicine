de7274a9cbd75c8f57ba2898e7822852a0e6c98b
Yaoyu917 <snsivic@gmail.com>
2025-11-30 23:05:15 +0800

    footer

--- src/app/patientmanage/page.js
+++ src/app/patientmanage/page.js
@@ -167,6 +167,14 @@ export default function DoctorPatientList() {
           )}
         </div>
       </div>
+    {/* Footer */}
+        <div className="bg-gray-800 text-white py-8">
+          <div className="max-w-7xl mx-auto px-4 text-center">
+            <p className="text-gray-400">
+              © 2025 MedOnGo 醫師平台. 讓醫療服務更便捷、更專業。
+            </p>
+          </div>
+        </div>
     </div>
   );
 }
