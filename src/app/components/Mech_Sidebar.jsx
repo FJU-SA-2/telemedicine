4265b0f46fb4b28a870fa1be69243649f161698a
xuan05419 <yutin0419@gmail.com>
2026-02-23 20:14:48 +0800

    side

--- src/app/components/Mech_Sidebar.jsx
+++ src/app/components/Mech_Sidebar.jsx
@@ -21,7 +21,7 @@ export default function DoctorSidebar({ isOpen, setIsOpen, approvalStatus }) {
   const pathname = usePathname();
 
   const menuItems = [
-    { id: "/doctorpage", label: "首頁", icon: House, href: "/methpage" },
+    { id: "/mechpage", label: "首頁", icon: House, href: "/mechpage" },
     { id: "/dintroduction", label: "使用流程介紹", icon: FileText, href: "/dintroduction", requiresAuth: false },
     { id: "/schedules", label: "排班管理", icon: CalendarClock, href: "/schedules", },
     { id: "/management", label: "預約管理", icon: UserRoundCheck, href: "/management",},
