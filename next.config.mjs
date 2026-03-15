56b72c00060ccab2aa2a8af2df7696b10d8c3606
Optau <a0979796361@gmail.com>
2026-03-15 23:27:41 +0800

    測試

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000";
 
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,  // 加這行
+  },
+  typescript: {
+    ignoreBuildErrors: true,   // 加這行
+  },
   async rewrites() {
     return [
       { source: "/api/:path*", destination: `${API_URL}/api/:path*` },
