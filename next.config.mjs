f603755dcb9be0824326d0695e528822bc407eda
Optau <a0979796361@gmail.com>
2026-03-15 23:46:27 +0800

    測試4.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000";
-
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,  // 加這行
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true,   // 加這行
+    ignoreBuildErrors: true,
   },
   async rewrites() {
     return [
-      { source: "/api/:path*", destination: `${API_URL}/api/:path*` },
-      { source: "/uploads/:path*", destination: `${API_URL}/uploads/:path*` },
+      { source: "/api/:path*", destination: `${process.env.BACKEND_URL}/api/:path*` },
+      { source: "/uploads/:path*", destination: `${process.env.BACKEND_URL}/uploads/:path*` },
     ];
   },
 };
