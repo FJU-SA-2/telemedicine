abd1caa657899a77f6f9b92ffb6ffe7dfa60ee8c
Yaoyu917 <snsivic@gmail.com>
2026-05-14 04:35:20 +0800

    機構醫生介紹及修改功能

--- src/app/components/Mech_Sidebar.jsx
+++ src/app/components/Mech_Sidebar.jsx
@@ -22,6 +22,7 @@ export default function DoctorSidebar({ isOpen, setIsOpen, approvalStatus }) {
 
   const menuItems = [
     { id: "/mechpage", label: "首頁", icon: House, href: "/mechpage" },
+    { id: "/doctors", label: "醫生介紹", icon: UserRoundCheck, href: "/mechpage/doctorlist" }, 
     { id: "/schedules", label: "排班管理", icon: CalendarClock, href: "/mechpage/schedule", },
     { id: "/record", label: "預約紀錄", icon: ClipboardList, href: "/recordmech",},
     { id: "/mechfeedback", label: "問題回報", icon: MessageCircleMore, href: "/mechfeedback" },
